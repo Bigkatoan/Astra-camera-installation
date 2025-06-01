49956e72b0c5df41b4f26bae195949a3e98fdcd1
Trần Đăng An <63138206+Bigkatoan@users.noreply.github.com>
2025-06-01 10:33:11 +0700

    Update astra_camera.py

--- astra_camera.py
+++ astra_camera.py
@@ -57,11 +57,13 @@ class Camera:
     def get_depth(self):
         frame = self.depth_stream.read_frame()
         frame_data = frame.get_buffer_as_uint16()
-        depthPix = np.frombuffer(frame_data, dtype=np.uint16)
-        depthPix.shape = (1, self.height, self.width)
-        depthPix = np.swapaxes(depthPix, 0, 2)
-        depthPix = np.swapaxes(depthPix, 0, 1)
-        return depthPix
+        # Put the depth frame into a numpy array and reshape it
+        img = np.frombuffer(frame_data, dtype=np.uint16)
+        img.shape = (1, 480, 640)
+        img = np.concatenate((img, img, img), axis=0)
+        img = np.swapaxes(img, 0, 2)
+        img = np.swapaxes(img, 0, 1)
+        return img
     
     def get_color(self):
         frame = self.color_stream.read_frame()
